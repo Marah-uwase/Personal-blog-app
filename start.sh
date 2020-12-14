05671b5d9cb02cf11825673145f559a688cd4e55
Marah-uwase <maranahuwase12@gmail.com>
2020-12-14 14:49:09 +0200

    exporting the sql db-uri

---
+++ start.sh
@@ -0,0 +1,9 @@
+# export NEWS_API_KEY='2ca104e7d5244b1eb8c43c144960e0dd'
+export SECRET_KEY= 'marah'
+export MAIL_USERNAME= 'maranahuwase12@gmail.com'
+export MAIL_PASSWORD= 'marahuwase@31998'
+export SQLALCHEMY_DATABASE_URI= 'postgresql://maranatha:marah@localhost/blogapp'
+export QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+
+python3.6 manage.py server
